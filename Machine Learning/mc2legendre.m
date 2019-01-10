1c2eb65d657c8cbcf61a46a239c1257987014664
Romain Fournier <rmn.fournier@gmail.com>
2019-01-10 11:04:16 +0100

    feature : Get legendre coefficients

--- Machine Learning/mc2legendre.m
+++ Machine Learning/mc2legendre.m
@@ -8,13 +8,18 @@
 % Therefore, we must import all file to get the time-correlation function
 
 % Tune the variables to your particular situation
+
+% 1 General 
+f=figure;
+check_plot = true ; % True if we want to see the data
+simulation_name="harmonic_oscillator"; % for figure names
+save_folder = "harm_osc_results/"; % Folder in which we save the figures and coefficients
+%2 MonteCarlo
 folder = "../MaxEnt/harm_osc/"; % Folder in which the files are 
 filename_prefix = "simulation"; % Prefix (common to all files)
 nb_timesteps = 31 ; % The total number of timesteps
-check_plot = true ; % True if we want to see the data
-simulation_name="harmonic_oscillator"; % for figure names
-save_folder = "harm_osc_results/"; % Folder in which we save the figures
-
+%3 Legendre parameters
+NB_GLs = [8,16,32]; % array of number of coefficients to test (to know how many of them we need)
 % No need to modify the next lines 
 mean_data=[];
 var_data=[];
@@ -30,13 +35,48 @@ for i=0:nb_timesteps
     end
     tau=[tau,(tmp(:,1))];
 end
-
 if(check_plot)
-    f=figure;
+    subplot(1,2,2)
+    hold on
     errorbar(tau,mean_data,(var_data))
-    xlabel("\tau")
-    ylabel("C^i(\tau)");
+end
+
+%% Legendre transform 
+% We have the function C^i(\tau), but the neural networks take its legendre
+% coefficients as input. We need to get them. 
+for NB_GL = NB_GLs
+
+    nl_handler=@(l) trapz(linspace(0,1,nb_timesteps+1),mean_data.*legendreP(l,2*(linspace(0,1,nb_timesteps+1))-1));
+    nl=zeros(NB_GL,1);
+    for i  = (0:(NB_GL-1))
+       nl(i+1)= nl_handler(i);
+    end
+    if(check_plot)
+        subplot(1,2,1)
+        plot(nl)
+        xlabel("Coefficient")
+        ylabel("G_l")
+        title("Legendre Coef")
+    end
+
+    %% Reconstruct the time correlation function from legendre coefficient
+    % We want to make sure that we have "good" information about the original
+    % function
+    c_i_reconstructed = zeros(1,nb_timesteps+1);
+    for i = 0 : NB_GL-1
+        c_i_reconstructed = c_i_reconstructed + nl(i+1)*legendreP(i,2*(linspace(0,1,nb_timesteps+1))-1);
+    end
+    if(check_plot)
+        subplot(1,2,2)
+        hold on
+        plot(tau,c_i_reconstructed)
+        xlabel("\tau")
+        ylabel("c_i Legendre")
+        title("Imaginary Time Correlation")
+    end
+end
+%% Save the figure
+if(check_plot)
     print_figure(f,save_folder+simulation_name+"_correlation",12,6);
 end
 
-%% 
