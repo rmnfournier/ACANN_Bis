310a8dc4182b142f3edc483d60c6582280793959
Romain Fournier <rmn.fournier@gmail.com>
2019-01-23 14:19:02 +0100

    feature : more subplot to figure 1

--- Figures/figure1.m
+++ Figures/figure1.m
@@ -6,9 +6,9 @@
 nb_coef=10;
 beta=0.25;
 tau= linspace(0,beta,1024)';
-g_to_plot=5;
+g_to_plot=[17,22,71,100];
 f=figure;
-replot=true;
+replot=false;
 
 %% Explain variance ratio
 if replot
@@ -20,7 +20,7 @@ individual_contributions = evr;
 for i=2:length(evr)
     individual_contributions(i)=individual_contributions(i)-sum(individual_contributions(1:(i-1)));
 end
-subplot(1,2,1)
+subplot(2,2,3)
 bar(individual_contributions(1:nb_coef));
 hold on
 %stairs(evr(1:nb_coef),'LineWidth',1.4)
@@ -33,20 +33,45 @@ set(gca,'YScale','log');
 %% Data example
 if replot
     G=importdata("figure1_G.csv");
+    A=importdata("figure1_A.csv");
 else
     G = importdata("../Data/lennard_jones/G_validation.csv");
+    A=importdata("../Data/lennard_jones/A_validation.csv");
     G=G(g_to_plot,:);
+    A=A(g_to_plot,:);
 end
-subplot(1,2,2);
+subplot(2,2,1);
 plot(tau,-G,'LineWidth',1.2);
 xlabel("\tau")
 ylabel("G(\tau)")
 
+subplot(2,2,2);
+plot(linspace(0,25,1024/2),A(:,1:512),'LineWidth',1.2);
+xlabel("\omega")
+ylabel("I(\omega)")
+
+%% Scatter plot
+if replot
+    G_r=importdata("figure1_G_r.csv");
+else
+    G_r = importdata("../Data/lennard_jones/G_validation_reduced.csv");
+end
+
+subplot(2,2,4)
+scatter3(G_r(:,1),G_r(:,2),G_r(:,3),'filled')
+xlabel("\lambda_1")
+ylabel("\lambda_2")
+zlabel("\lambda_3")
+box on
+grid on
+
 %% Save figure and data
-print_figure(f,'database',6.4,3.2);
+print_figure(f,'database',8.8,6.6);
+if ~replot
 dlmwrite('figure1_ind_contr.csv',individual_contributions,'delimiter',',');
 dlmwrite('figure1_evr.csv',evr,'delimiter',',','precision','%.15f');
 dlmwrite('figure1_G.csv',G,'delimiter',',');
 dlmwrite('figure1_tau.csv',tau,'delimiter',',');
-
+dlmwrite('figure1_G_r.csv',G_r,'delimiter',',');
+end
 
